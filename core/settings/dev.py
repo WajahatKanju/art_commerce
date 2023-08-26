876ddb548bada08c28a84507d5674b6ab36fc716
WajahatKanju <wajahat5ahmad@gmail.com>
2023-08-27 00:03:42 +0500

    inital Commit

---
+++ core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zw6h0cz4jt50=$$azf-bh3x^za2l$zxp(im7_7@p5sv0ik!uwn"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
